2b88f985232c3ddcf55c7bc953aa6accb49236f2
Yachika Sharma <shakuntalaramphalsharma@gmail.com>
2026-05-24 16:33:25 +0530

    Add Expense model with migrations

--- ExpenseTracker/models.py
+++ ExpenseTracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Expense(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category= models.CharField(max_length=70)
+    description = models.CharField(max_length=400)
+    date=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
