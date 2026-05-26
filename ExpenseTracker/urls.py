81db9c8cafbf0ee434f61a35b495cf3d8fb47f6f
Yachika Sharma <shakuntalaramphalsharma@gmail.com>
2026-05-26 22:19:37 +0530

    refactor: convert expense URLs to CBV

--- ExpenseTracker/urls.py
+++ ExpenseTracker/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-
+from .views import ExpenseListView, AddExpense, UpdateExpense, DeleteExpense
 urlpatterns = [
-    path('',views.list_expense,name='expense_list'),
-    path('add/',views.add_expense,name='add_expense'),
+    path('',views.cover,name='cover'),
+    path('expense/',ExpenseListView.as_view(),name='expense_list'),
+    path('add/',AddExpense.as_view(),name='add_expense'),
     path('register/',views.register, name='register'),
-    path('edit/<int:pk>/',views.edit_expense,name='edit'),
-    path('delete/<int:pk>/',views.delete_expense,name='delete'),
+    path('edit/<int:pk>/',UpdateExpense.as_view(),name='edit'),
+    path('delete/<int:pk>/',DeleteExpense.as_view(),name='delete'),
 ]
