cbf1e25e0286fec73ed8abc071d91c76594cb844
Yachika Sharma <shakuntalaramphalsharma@gmail.com>
2026-05-28 22:24:15 +0530

    fix: redirect to cover page on logout

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
